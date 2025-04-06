890f316c09d44a5ebb43767085b750c23b3a5d11
maki.xie <makixie1@outlook.com>
2025-04-06 13:53:26 -0400

    refine consensus and cli together

--- consensus.go
+++ consensus.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"context"
-	"fmt"
+
+	"github.com/maki3cat/mkraft/rpc"
 )
 
-// CONSENSUS MODULE combines the rpc clients with consensus
+type MajorityAppendEntriesResp struct {
+	Term            int
+	Success         bool
+	SingleResponses []rpc.AppendEntriesResponse
+	OriginalRequest rpc.AppendEntriesRequest
+}
 
-// todo: should use next layer RPC keep calling the unreturned nodes until the context is canceled
-// only send to the channel when majority is reached OR current candidate shall end
-func RequestVoteSend(ctx context.Context, request RequestVoteRequest, resultChannel chan MajorityRequestVoteResp) {
-	// todo: should add a wrapper to retry until cancel
-	members := getMembership()
+type MajorityRequestVoteResp struct {
+	Term            int
+	VoteGranted     bool
+	SingleResponses []rpc.RequestVoteResponse
+	OriginalRequest rpc.RequestVoteRequest
+	Error           error
+}
 
-	// maki: important for testing
-	// todo: if the ctx is canceled by the fan-out not returned, will
-	// the goroutine write to the channel?
-	var resChan = make(chan ResWrapper, len(members))
-	rpcCall := func(membershipID int) {
-		resp, err := RPCClientSendRequestVote(ctx, membershipID, request)
-		wrapper := ResWrapper{
-			Err:  err,
-			Resp: resp,
-		}
-		resChan <- wrapper
-	}
+// CONSENSUS MODULE
+// todo: currently the result channel only retruns when there is win/fail for sure
+func RequestVoteSend(ctx context.Context, request rpc.RequestVoteRequest, resultChannel chan MajorityRequestVoteResp) {
+
+	members := getClientOfAllMembers()
+	resChan := make(chan rpc.RPCResWrapper[rpc.RequestVoteResponse], len(members)) // buffered with len(members) to prevent goroutine leak
 
 	// FAN-OUT
 	for _, member := range members {
-		go rpcCall(member)
-		fmt.Println("send request vote to node", member)
+		go member.RetriedSendRequestVote(ctx, request, resChan)
 	}
 
 	// FAN-IN WITH STOPPING SHORT
@@ -37,26 +38,26 @@ func RequestVoteSend(ctx context.Context, request RequestVoteRequest, resultChan
 	majority := len(members)/2 + 1
 	voteAccumulated := 0
 	voteFailed := 0
+	// todo: or should use for true range?
 	for i := 0; i < len(members); i++ {
 		select {
 		case res := <-resChan:
 			if err := res.Err; err != nil {
-				// todo: further analysis what happens in error -> lost one vote/heartbeat
-				sugarLogger.Info("error in sending request vote", err)
+				sugarLogger.Error("error in sending request vote to one node", err)
 				continue
 			} else {
-				term := res.Resp.(RequestVoteResponse).Term
+				resp := res.Resp
 				// if someone responds with a term greater than the current term
-				if term > request.Term {
+				if resp.Term > request.Term {
 					sugarLogger.Info("term is greater than current term")
 					resultChannel <- MajorityRequestVoteResp{
-						Term:        term,
+						Term:        resp.Term,
 						VoteGranted: false,
 					}
 					return
 				}
-				if term == request.Term {
-					if res.Resp.(RequestVoteResponse).VoteGranted {
+				if resp.Term == request.Term {
+					if resp.VoteGranted {
 						// won the election
 						voteAccumulated++
 						if voteAccumulated >= majority {
@@ -67,7 +68,8 @@ func RequestVoteSend(ctx context.Context, request RequestVoteRequest, resultChan
 							return
 						}
 					} else {
-						// a fail or draw in the election
+						// todo: not sure if this the right logic to do this
+						// a fail or draw in the election, unsure
 						// no need to return anything
 						voteFailed++
 						if voteFailed > total-majority {
@@ -75,9 +77,9 @@ func RequestVoteSend(ctx context.Context, request RequestVoteRequest, resultChan
 						}
 					}
 				}
-				if term < request.Term {
-					// todo: how to handle somecase that should never happen, should it panic?
-					sugarLogger.Error("term is less than current term, this should not happen")
+				if resp.Term < request.Term {
+					sugarLogger.Error("invairant failed, smaller term is not overwritten by larger term")
+					panic("this should not happen, the consensus algorithm is not implmented correctly")
 				}
 			}
 		case <-ctx.Done():
@@ -89,10 +91,60 @@ func RequestVoteSend(ctx context.Context, request RequestVoteRequest, resultChan
 }
 
 func AppendEntriesSend(
-	ctx context.Context, request AppendEntriesRequest, respChan chan MajorityAppendEntriesResp, errChan chan error) {
-	// maki: some implementation level design details to be documented
-	// SEND TO ALL OTHER NODES
-	// todo: implementation problem
-	// synchronous OR asynchronous call
-	fmt.Println("send heartbeat to all other nodes")
+	ctx context.Context, request rpc.AppendEntriesRequest, respChan chan MajorityAppendEntriesResp, errChan chan error) {
+	members := getClientOfAllMembers()
+	resChan := make(chan rpc.RPCResWrapper[rpc.AppendEntriesResponse], len(members)) // buffered with len(members) to prevent goroutine leak
+	// FAN-OUT
+	for _, member := range members {
+		go member.RetriedSendAppendEntries(ctx, request, resChan)
+	}
+	// FAN-IN WITH STOPPING SHORT
+	total := len(members)
+	majority := len(members)/2 + 1
+	successAccumulated := 0
+	successFailed := 0
+	for i := 0; i < len(members); i++ {
+		select {
+		case res := <-resChan:
+			if err := res.Err; err != nil {
+				sugarLogger.Error("error in sending append entries to one node", err)
+				continue
+			} else {
+				resp := res.Resp
+				if resp.Term > request.Term {
+					sugarLogger.Info("term is greater than current term")
+					respChan <- MajorityAppendEntriesResp{
+						Term:    resp.Term,
+						Success: false,
+					}
+					return
+				}
+				if resp.Term == request.Term {
+					if resp.Success {
+						successAccumulated++
+						if successAccumulated >= majority {
+							respChan <- MajorityAppendEntriesResp{
+								Term:    request.Term,
+								Success: true,
+							}
+							return
+						}
+					} else {
+						successFailed++
+						if successFailed > total-majority {
+							sugarLogger.Error("invairant failed, one same term has different leader?")
+							panic("this should not happen, the consensus algorithm is not implmented correctly")
+						}
+					}
+				}
+				if resp.Term < request.Term {
+					sugarLogger.Error("invairant failed, smaller term is not overwritten by larger term")
+					panic("this should not happen, the consensus algorithm is not implmented correctly")
+				}
+			}
+		case <-ctx.Done():
+			sugarLogger.Info("context canceled")
+			return
+		}
+	}
 }
--- rpc/cli.go
+++ rpc/cli.go
@@ -1,6 +1,13 @@
 // This file defines the RPC client interface and its implementation
 // the RPC client is responsible for sending the RPC requests to only one other node and try to get a response
 // it doesn't manage the consensus between different nodes
+// TODO: maki: need to learn and revist the future and retry pattern here
+// golang gymnastics
+// summarize the golang patterns here:
+// 1) async call with future; 2) retry pattern; 3) context with timeout/cancel; 4)template and generics
+
+// paper: the 2 RPCs defined by the Raft paper
+// membership module shall be responsible for the client to each member
 
 package rpc
 
@@ -12,18 +19,11 @@ import (
 	"github.com/maki3cat/mkraft/conf"
 )
 
-// TODO: maki: need to learn and revist the future and retry pattern here
-// golang gymnastics
-// summarize the golang patterns here:
-// 1) async call with future; 2) retry pattern; 3) context with timeout/cancel; 4)template and generics
-
-// paper: the 2 RPCs defined by the Raft paper
-// membership module shall be responsible for the client to each member
+// the real RPC iface witch will handle cancel/timeout with network solutions
+// will plugin in gRPC
 type RPCClientIface interface {
 	SyncSendRequestVote(ctx context.Context, req RequestVoteRequest) (RequestVoteResponse, error)
 	SyncSendAppendEntries(ctx context.Context, req AppendEntriesRequest) (AppendEntriesResponse, error)
-	AsyncSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse]
-	AsyncSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse]
 }
 
 func NewRPCClient() RPCClientIface {
@@ -50,41 +50,12 @@ func (mock *MockSimpleRPCClientImpl) SyncSendAppendEntries(ctx context.Context, 
 	}, nil
 }
 
-// MAKI: NOT SURE IF THIS IS A PATTERN
-// maki: how to combine the ctx timeout/cancel select and this simiple waiting
-// maki: is this the simple future pattern in golang
-// maki: todo: how to stop the synchronous call when timeout? -> check how RPC implements this like grpc?
-func (mock *MockSimpleRPCClientImpl) AsyncSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse] {
-	res := make(chan RPCResWrapper[RequestVoteResponse], 1) // buffered with 1 to prevent goroutine leak
-	go func() {
-		response, err := mock.SyncSendRequestVote(ctx, req)
-		wrapper := RPCResWrapper[RequestVoteResponse]{
-			Resp: response,
-			Err:  err,
-		}
-		res <- wrapper
-	}()
-	return res
-}
-
-func (mock *MockSimpleRPCClientImpl) AsyncSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse] {
-	res := make(chan RPCResWrapper[AppendEntriesResponse], 1)
-	go func() {
-		response, err := mock.SyncSendAppendEntries(ctx, req)
-		wrapper := RPCResWrapper[AppendEntriesResponse]{
-			Resp: response,
-			Err:  err,
-		}
-		res <- wrapper
-	}()
-	return res
-}
-
-// the complicated RPC client used directly for the server
-// TODO: shall merge the iface of the simple client and the retried client
+// the real RPC wrapper used directly for the server
 type RetriedClientIface interface {
-	RetriedSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse]
-	RetriedSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse]
+	RetriedSendAppendEntries(ctx context.Context, req AppendEntriesRequest, wrappedResChan chan RPCResWrapper[AppendEntriesResponse])
+	RetriedSendRequestVote(ctx context.Context, req RequestVoteRequest, wrappedResChan chan RPCResWrapper[RequestVoteResponse])
+	AsyncSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse]
+	AsyncSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse]
 }
 
 func NewRetriedClient(simpleClient RPCClientIface) RetriedClientIface {
@@ -101,51 +72,73 @@ type RetriedRPCFunc[TReq any, TRes RPCResponse] func(ctx context.Context, req TR
 type AsyncRPCFunc[TReq any, TRes RPCResponse] func(ctx context.Context, req TReq) chan RPCResWrapper[TRes]
 
 // Using the template to create the specific RPC functions
-func (rc *RetriedClientImpl) RetriedSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse] {
-	return retriedRPCFunc(rc.simpleClient.AsyncSendAppendEntries, ctx, req)
+func (rc *RetriedClientImpl) RetriedSendAppendEntries(ctx context.Context, req AppendEntriesRequest, wrappedResChan chan RPCResWrapper[AppendEntriesResponse]) {
+	retriedRPCFunc(rc.AsyncSendAppendEntries, ctx, req, wrappedResChan)
 }
 
-func (rc *RetriedClientImpl) RetriedSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse] {
-	return retriedRPCFunc(rc.simpleClient.AsyncSendRequestVote, ctx, req)
+func (rc *RetriedClientImpl) RetriedSendRequestVote(ctx context.Context, req RequestVoteRequest, wrappedResChan chan RPCResWrapper[RequestVoteResponse]) {
+	retriedRPCFunc(rc.AsyncSendRequestVote, ctx, req, wrappedResChan)
 }
 
 // CreateRetriedRPCFunc creates a retry function for AppendEntries or RequestVote RPCs
 func retriedRPCFunc[TReq any, TRes RPCResponse](
-	asyncFunc AsyncRPCFunc[TReq, TRes], ctx context.Context, req TReq,
-) chan RPCResWrapper[TRes] {
+	asyncFunc AsyncRPCFunc[TReq, TRes], ctx context.Context, req TReq, wrappedResChan chan RPCResWrapper[TRes]) {
+	defer close(wrappedResChan)
 
-	wrappedResChan := make(chan RPCResWrapper[TRes])
-	go func() {
-		defer close(wrappedResChan)
+	retryTicker := time.NewTicker(time.Millisecond * conf.RPC_REUQEST_TIMEOUT_IN_MS)
+	defer retryTicker.Stop()
 
-		retryTicker := time.NewTicker(time.Millisecond * conf.RPC_REUQEST_TIMEOUT_IN_MS)
-		defer retryTicker.Stop()
+	var singleResChan chan RPCResWrapper[TRes]
+	var singleCallCtx context.Context
+	var singleCallCancel context.CancelFunc
 
-		var singleResChan chan RPCResWrapper[TRes]
-		var singleCallCtx context.Context
-		var singleCallCancel context.CancelFunc
-		singleCallCtx, singleCallCancel = context.WithTimeout(ctx, time.Millisecond*(conf.RPC_REUQEST_TIMEOUT_IN_MS-10))
-		singleResChan = asyncFunc(singleCallCtx, req)
+	singleCallCtx, singleCallCancel = context.WithTimeout(ctx, time.Millisecond*(conf.RPC_REUQEST_TIMEOUT_IN_MS-10))
+	singleResChan = asyncFunc(singleCallCtx, req)
 
-		for {
-			select {
-			case <-ctx.Done():
-				// Parent context canceled or timed out
-				singleCallCancel()
-				wrappedResChan <- RPCResWrapper[TRes]{Err: fmt.Errorf("context done")}
-				return
-			case <-retryTicker.C:
-				// No response within timeout, retry
-				singleCallCancel()
-				singleCallCtx, singleCallCancel = context.WithTimeout(ctx, time.Millisecond*conf.RPC_REUQEST_TIMEOUT_IN_MS)
-				singleResChan = asyncFunc(singleCallCtx, req)
-			case response := <-singleResChan:
-				// Got a response
-				singleCallCancel()
-				wrappedResChan <- response
-				return
-			}
+	for {
+		select {
+		case <-ctx.Done():
+			// Parent context canceled or timed out
+			singleCallCancel()
+			wrappedResChan <- RPCResWrapper[TRes]{Err: fmt.Errorf("context done")}
+			return
+		case <-retryTicker.C:
+			// No response within timeout, retry
+			singleCallCancel()
+			singleCallCtx, singleCallCancel = context.WithTimeout(ctx, time.Millisecond*conf.RPC_REUQEST_TIMEOUT_IN_MS)
+			singleResChan = asyncFunc(singleCallCtx, req)
+		case response := <-singleResChan:
+			// Got a response
+			singleCallCancel()
+			wrappedResChan <- response
+			return
 		}
+	}
+}
+
+func (rc *RetriedClientImpl) AsyncSendRequestVote(ctx context.Context, req RequestVoteRequest) chan RPCResWrapper[RequestVoteResponse] {
+	res := make(chan RPCResWrapper[RequestVoteResponse], 1) // buffered with 1 to prevent goroutine leak
+	go func() {
+		// todo: this should be the real rpc
+		response, err := rc.simpleClient.SyncSendRequestVote(ctx, req)
+		wrapper := RPCResWrapper[RequestVoteResponse]{
+			Resp: response,
+			Err:  err,
+		}
+		res <- wrapper
 	}()
-	return wrappedResChan
+	return res
+}
+
+func (rc *RetriedClientImpl) AsyncSendAppendEntries(ctx context.Context, req AppendEntriesRequest) chan RPCResWrapper[AppendEntriesResponse] {
+	res := make(chan RPCResWrapper[AppendEntriesResponse], 1)
+	go func() {
+		response, err := rc.simpleClient.SyncSendAppendEntries(ctx, req)
+		wrapper := RPCResWrapper[AppendEntriesResponse]{
+			Resp: response,
+			Err:  err,
+		}
+		res <- wrapper
+	}()
+	return res
 }
