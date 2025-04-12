eb11ff3caeb68b7a11762d2832d9427beefa5b11
maki.xie <makixie1@outlook.com>
2025-04-12 11:28:47 -0400

    refine configuration

--- util/configuration.go
+++ util/configuration.go
@@ -2,92 +2,109 @@ package util
 
 import (
 	"encoding/json"
-	"fmt"
 	"math/rand"
 	"time"
 )
 
+var (
+	theConf *Config
+)
+
+// maki: haven't found easy way to lock the writing of theConf without copying the value
+// so careful coding is needed
+func GetConfig() *Config {
+	return theConf
+}
+
+// todo: in the near future, we need update membership dynamically;
+// todo: in the remote future, we need to update the config dynamically
+func InitConf(membership *MembershipConfig) {
+	theConf = CreateDefaultConf()
+	theConf.MembershipConfig = *membership
+	sugarLogger.Infof("init the config to %s", theConf)
+}
+
+func CreateDefaultConf() *Config {
+	return &Config{
+		RPCRequestTimeoutInMs:     RPC_REUQEST_TIMEOUT_IN_MS,
+		ElectionTimeoutMinInMs:    ELECTION_TIMEOUT_MIN_IN_MS,
+		ElectionTimeoutMaxInMs:    ELECTION_TIMEOUT_MAX_IN_MS,
+		LeaderHeartbeatPeriodInMs: LEADER_HEARTBEAT_PERIOD_IN_MS,
+		LeaderBufferSize:          LEADER_BUFFER_SIZE,
+		ClientCommandBufferSize:   CLIENT_COMMAND_BUFFER_SIZE,
+		ClientCommandBatchSize:    CLIENT_COMMAND_BATCH_SIZE,
+	}
+}
+
+// DEFAULT VALUES of configuration
+const CLIENT_COMMAND_BATCH_SIZE = 10
+const CLIENT_COMMAND_BUFFER_SIZE = 1000
+
+const LEADER_BUFFER_SIZE = 1000
 const LEADER_HEARTBEAT_PERIOD_IN_MS = 100
-const REUQEST_TIMEOUT_IN_MS = 200
+
 const RPC_REUQEST_TIMEOUT_IN_MS = 200
 
-// This non-determinisim is by design
-// because sometimes randomness brings simplicity
-// here it minimizes the possiblity of contention of leader and split vote
 const ELECTION_TIMEOUT_MIN_IN_MS = 150
 const ELECTION_TIMEOUT_MAX_IN_MS = 350
 
-func GetRandomElectionTimeout() time.Duration {
-	diff := ELECTION_TIMEOUT_MAX_IN_MS - ELECTION_TIMEOUT_MIN_IN_MS
+type Config struct {
+	// RPC timeout
+	RPCRequestTimeoutInMs int `json:"rpc_request_timeout_in_ms"`
+
+	// Election timeout
+	ElectionTimeoutMinInMs int `json:"election_timeout_min_in_ms"`
+	ElectionTimeoutMaxInMs int `json:"election_timeout_max_in_ms"`
+
+	// Leader
+	LeaderHeartbeatPeriodInMs int `json:"leader_heartbeat_period_in_ms"`
+	LeaderBufferSize          int `json:"leader_buffer_size"`
+
+	// Client
+	ClientCommandBufferSize int `json:"client_command_buffer_size"`
+	ClientCommandBatchSize  int `json:"client_command_batch_size"`
+
+	// Node Meta
+	MembershipConfig
+}
+
+func (c *Config) GetRPCRequestTimeout() time.Duration {
+	return time.Duration(c.RPCRequestTimeoutInMs) * time.Millisecond
+}
+
+func (c *Config) GetElectionTimeout() time.Duration {
+	diff := c.ElectionTimeoutMaxInMs - c.ElectionTimeoutMinInMs
 	randomMs := rand.Intn(diff) + ELECTION_TIMEOUT_MIN_IN_MS
 	return time.Duration(randomMs) * time.Millisecond
 }
 
-const HANDLE_CLIENT_COMMAND_BATCH = 10
-const HANDLE_CLIENT_COMMAND_BUFFER = 1000
-
-// leader configuration
-const LEADER_BUFFER_SIZE = 1000
-
-type Configuration struct {
-	// RPC timeout
-	RPCRequestTimeout     time.Duration `json:"rpc_request_timeout"`
-	RPCRequestTimeoutInMs int           `json:"rpc_request_timeout_in_ms"`
-
-	// Election timeout
-	ElectionTimeout     time.Duration `json:"election_timeout"`
-	ElectionTimeoutInMs int           `json:"election_timeout_in_ms"`
-
-	// Leader
-	LeaderHeartbeatPeriod     time.Duration `json:"leader_heartbeat_period"`
-	LeaderHeartbeatPeriodInMs int           `json:"leader_heartbeat_period_in_ms"`
-	LeaderBufferSize          int           `json:"leader_buffer_size"`
-
-	// Client
-	ClientCommandBufferSize int `json:"client_command_buffer_size"`
-
-	// Node Meta
-	MembershipBasicInfo
+func (c *Config) GetLeaderHeartbeatPeriod() time.Duration {
+	return time.Duration(c.LeaderHeartbeatPeriodInMs) * time.Millisecond
 }
 
-func (c *Configuration) String() string {
+func (c *Config) GetLeaderBufferSize() int {
+	return c.LeaderBufferSize
+}
+
+func (c *Config) GetClientCommandBufferSize() int {
+	return c.ClientCommandBufferSize
+}
+
+func (c *Config) GetClientCommandBatchSize() int {
+	return c.ClientCommandBatchSize
+}
+
+func (c *Config) String() string {
 	jsonStr, _ := json.Marshal(c)
 	return string(jsonStr)
 }
 
-type MembershipBasicInfo struct {
-	NodeID     string              `json:"node_id"`
-	Membership []NodeConfigruation `json:"membership"`
+type MembershipConfig struct {
+	NodeID     string       `json:"node_id"`
+	Membership []NodeConfig `json:"membership"`
 }
 
-type NodeConfigruation struct {
+type NodeConfig struct {
 	NodeID  string `json:"node_id"`
 	NodeURI string `json:"node_uri"`
-}
-
-var (
-	theConfig *Configuration
-)
-
-func GetConfig() *Configuration {
-	return theConfig
-}
-
-func SetConfig(membership *MembershipBasicInfo) {
-	// todo: add sync to config
-	theConfig = GetDefaultConfig()
-	theConfig.MembershipBasicInfo = *membership
-	fmt.Printf("set config: %s\n", theConfig)
-}
-
-func GetDefaultConfig() *Configuration {
-	return &Configuration{
-		RPCRequestTimeout:         time.Duration(RPC_REUQEST_TIMEOUT_IN_MS) * time.Millisecond,
-		RPCRequestTimeoutInMs:     RPC_REUQEST_TIMEOUT_IN_MS,
-		ElectionTimeoutInMs:       ELECTION_TIMEOUT_MIN_IN_MS,
-		LeaderHeartbeatPeriod:     time.Duration(LEADER_HEARTBEAT_PERIOD_IN_MS) * time.Millisecond,
-		LeaderHeartbeatPeriodInMs: LEADER_HEARTBEAT_PERIOD_IN_MS,
-		ClientCommandBufferSize:   HANDLE_CLIENT_COMMAND_BUFFER,
-		LeaderBufferSize:          LEADER_BUFFER_SIZE,
-	}
 }
