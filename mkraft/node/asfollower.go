d906e2950e12fa02ddcbcfcddc0024092b5738a0
maki.xie <makixie1@outlook.com>
2025-05-29 15:51:10 -0400

    finish appendEntries for candidate

--- mkraft/node/asfollower.go
+++ mkraft/node/asfollower.go
@@ -10,7 +10,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// todo: add handle appendEntries from a leader which may delete/overwrite logs
 /*
 PAPER:
 
@@ -65,7 +64,7 @@ func (n *Node) RunAsFollower(ctx context.Context) {
 					electionTicker.Reset(n.cfg.GetElectionTimeout())
 				case req := <-n.appendEntryChan:
 					electionTicker.Stop()
-					resp := n.handlerAppendEntriesAsFollower(ctx, req.Req)
+					resp := n.handlerAppendEntriesAsNoLeader(ctx, req.Req)
 					wrappedResp := utils.RPCRespWrapper[*rpc.AppendEntriesResponse]{
 						Resp: resp,
 						Err:  nil,
@@ -78,8 +77,118 @@ func (n *Node) RunAsFollower(ctx context.Context) {
 	}
 }
 
-func (n *Node) handlerAppendEntriesAsFollower(ctx context.Context, req *rpc.AppendEntriesRequest) *rpc.AppendEntriesResponse {
-	defer n.updateCommitIdx(req.LeaderCommit)
+/*
+PAPER (quote):
+Shared Rule: if any RPC request or response is received from a server with a higher term,
+convert to follower
+How the Shared Rule works for Candidates:
+(1) handle the response of RequestVoteRPC initiated by itself
+(2) handle request of AppendEntriesRPC initiated by another server
+(3) handle reuqest of RequestVoteRPC initiated by another server
+
+Specifical Rule for Candidates:
+On conversion to a candidate, start election:
+(1) increment currentTerm
+(2) vote for self
+(3) send RequestVote RPCs to all other servers
+if votes received from majority of servers: become leader
+if AppendEntries RPC received from new leader: convert to follower
+if election timeout elapses: start new election
+*/
+func (n *Node) RunAsCandidate(ctx context.Context) {
+	if n.State != StateCandidate {
+		panic("node is not in CANDIDATE state")
+	}
+
+	n.logger.Info("node starts to acquiring CANDIDATE state")
+	n.sem.Acquire(ctx, 1)
+	n.logger.Info("node has acquired semaphore in CANDIDATE state")
+	defer n.sem.Release(1)
+
+	consensusChan := n.runOneElectionAsCandidate(ctx)
+
+	ticker := time.NewTicker(n.cfg.GetElectionTimeout())
+	for {
+		currentTerm := n.getCurrentTerm()
+		select {
+		case <-ctx.Done():
+			n.logger.Warn("raft node's main context done, exiting")
+			return
+		default:
+			{
+				select {
+				case <-ctx.Done():
+					n.logger.Warn("raft node main context done")
+					n.membership.GracefulShutdown()
+					return
+				case response := <-consensusChan: // some response from last election
+					// I don't think we need to reset the ticker here
+					// voteCancel() // cancel the rest
+					if response.VoteGranted {
+						n.State = StateLeader
+						go n.RunAsLeader(ctx)
+						return
+					} else {
+						if response.Term > currentTerm {
+							// some one has become a leader
+							// voteCancel()
+							err := n.storeCurrentTermAndVotedFor(response.Term, "") // did not vote for anyone
+							if err != nil {
+								n.logger.Error(
+									"error in storeCurrentTermAndVotedFor", zap.Error(err),
+									zap.String("nId", n.NodeId))
+								panic(err) // critical error, cannot continue
+							}
+							n.State = StateFollower
+							go n.RunAsFollower(ctx)
+							return
+						} else {
+							n.logger.Info(
+								"not enough votes, re-elect again",
+								zap.Int("term", int(currentTerm)), zap.String("nId", n.NodeId))
+						}
+					}
+				case <-ticker.C: // last election timeout withno response
+					// voteCancel()
+					consensusChan = n.runOneElectionAsCandidate(ctx)
+				case requestVoteInternal := <-n.requestVoteChan: // commonRule: handling voteRequest from another candidate
+					if requestVoteInternal.IsTimeout.Load() {
+						n.logger.Warn("request vote is timeout")
+						continue
+					}
+					req := requestVoteInternal.Req
+					resChan := requestVoteInternal.RespChan
+					resp := n.handleVoteRequest(req)
+					wrappedResp := utils.RPCRespWrapper[*rpc.RequestVoteResponse]{
+						Resp: resp,
+						Err:  nil,
+					}
+					resChan <- &wrappedResp
+					// this means other candiate has a higher term
+					if resp.VoteGranted {
+						n.State = StateFollower
+						go n.RunAsFollower(ctx)
+						return
+					}
+				case req := <-n.appendEntryChan: // commonRule: handling appendEntry from a leader which can be stale or new
+					resp := n.handlerAppendEntriesAsNoLeader(ctx, req.Req)
+					wrappedResp := utils.RPCRespWrapper[*rpc.AppendEntriesResponse]{
+						Resp: resp,
+						Err:  nil,
+					}
+					req.RespChan <- &wrappedResp
+					if req.Req.Term >= currentTerm { // maki: here allows the equal term, because current node is not leader
+						n.State = StateFollower
+						go n.RunAsFollower(ctx)
+						return
+					}
+				}
+			}
+		}
+	}
+}
+
+func (n *Node) handlerAppendEntriesAsNoLeader(ctx context.Context, req *rpc.AppendEntriesRequest) *rpc.AppendEntriesResponse {
 	var response rpc.AppendEntriesResponse
 	reqTerm := uint32(req.Term)
 	currentTerm := n.getCurrentTerm()
@@ -91,7 +200,11 @@ func (n *Node) handlerAppendEntriesAsFollower(ctx context.Context, req *rpc.Appe
 		return &response
 	}
 
-	// update the term
+	// the request is valid:
+	// 1. finally update the commit index
+	defer n.updateCommitIdx(req.LeaderCommit)
+
+	// 2. update the term
 	if reqTerm > currentTerm {
 		err := n.storeCurrentTermAndVotedFor(reqTerm, "") // did not vote for anyone
 		if err != nil {
@@ -102,7 +215,7 @@ func (n *Node) handlerAppendEntriesAsFollower(ctx context.Context, req *rpc.Appe
 		}
 	}
 
-	// append logs
+	// 3. append logs
 	logs := make([][]byte, len(req.Entries))
 	for idx, entry := range req.Entries {
 		logs[idx] = entry.Data
@@ -126,5 +239,34 @@ func (n *Node) handlerAppendEntriesAsFollower(ctx context.Context, req *rpc.Appe
 		// update the commit index
 		return &response
 	}
+}
 
+func (node *Node) runOneElectionAsCandidate(ctx context.Context) chan *MajorityRequestVoteResp {
+	ctx, requestID := common.GetOrGenerateRequestID(ctx)
+	consensusChan := make(chan *MajorityRequestVoteResp, 1)
+
+	err := node.updateCurrentTermAndVotedForAsCandidate()
+	if err != nil {
+		node.logger.Error(
+			"error in updateCurrentTermAndVotedForAsCandidate", zap.String("requestID", requestID), zap.Error(err))
+		panic(err) // critical error, cannot continue
+	}
+
+	req := &rpc.RequestVoteRequest{
+		Term:        node.getCurrentTerm(),
+		CandidateId: node.NodeId,
+	}
+	ctxTimeout, _ := context.WithTimeout(
+		ctx, time.Duration(node.cfg.GetElectionTimeout()))
+	go func() {
+		resp, err := node.ConsensusRequestVote(ctxTimeout, req)
+		if err != nil {
+			node.logger.Error(
+				"error in RequestVoteSendForConsensus", zap.String("requestID", requestID), zap.Error(err))
+			return
+		} else {
+			consensusChan <- resp
+		}
+	}()
+	return consensusChan
 }
